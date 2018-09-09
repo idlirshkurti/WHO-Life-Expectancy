9c8d6746bfff397ce2ef17103cfb9832ec8dcf5f
Idlir Shkurti <idlir.sh@gmail.com>
2018-09-09 14:02:34 +0200

    Update packages.R

--- packages.R
+++ packages.R
@@ -1,3 +1,4 @@
+# Call this from all the other scripts
 packages <- c('ggplot2', 'lattice', 'penalized', 'mice', 'rjags', 'corrplot', 'ggfortify',
  'reshape2', 'stats', 'dlm', 'grid', 'gridExtra','MCMCpack', 'stargazer', 'MASS', 'xtable')
 lapply(packages, require, character.only = TRUE)
